40a6a82d0b01de81b16206e723a40d827df254c0
LucaBn <luca.bonotti@virgilio.it>
2026-04-25 02:23:25 +0200

    refactor(getColorFromPlayerName): :recycle: update names and colors

--- src/utils/getColorFromPlayerName.ts
+++ src/utils/getColorFromPlayerName.ts
@@ -1,12 +1,14 @@
 export const getColorFromPlayerName = (name: string) => {
   const colorMap: Record<string, string> = {
-    Bono: "#FF6384",
     Bas: "#36A2EB",
+    Bono: "#FF6384",
     David: "#FFCE56",
+    Edo: "#221E90",
     Fra: "#4BC0C0",
     Frullo: "#9966FF",
-    Sara: "#FF9F40",
     Luca: "#F44336",
+    Pelle: "#008000",
+    Sara: "#FF9F40",
   };
 
   return colorMap[name] ?? "#9AA0A6";
